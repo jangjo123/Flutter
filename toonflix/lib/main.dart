484308359be212a94cb3a36d4cf11fe82baa7b40
jangjo123 <cjkh1324@naver.com>
2023-05-20 13:55:46 +0900

    Buttons Section

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -22,6 +22,7 @@ class App extends StatelessWidget{
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
               height: 80,
@@ -44,12 +45,49 @@ class App extends StatelessWidget{
                   style: TextStyle(
                     color: Colors.white.withOpacity(0.8),
                     fontSize: 18,
-                    ),
+                  ),
                   ),
                 ],
                 )
               ],
             ),
+            SizedBox(height: 120,),
+            Text(
+              "Totla Balance",
+              style:  TextStyle(
+                fontSize: 22,
+                color: Colors.white.withOpacity(0.8),
+                ),
+            ),
+            SizedBox(height: 5,),
+            Text(
+              "\$5 194 482",
+              style:  TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+                ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50,),
+                    child: Text(
+                      "Transfer",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),),
       ),
